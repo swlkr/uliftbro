387a3b9caac45f5e57b30bed02d5b29182bdf67c
Sean Walker <swlkr.rbl@gmail.com>
2023-12-17 12:45:04 -0700

    render html

---
+++ dubs/src/lib.rs
@@ -0,0 +1,116 @@
+pub use axum::{
+    async_trait,
+    extract::{FromRequestParts, Json, Query},
+    http::header::*,
+    http::request::Parts,
+    http::StatusCode,
+    routing, RequestPartsExt, Router,
+};
+pub use enum_router::Routes;
+pub mod tokio {
+    pub use tokio::*;
+}
+pub use serde::*;
+
+pub mod html {
+    use std::fmt::Display;
+
+    use axum::body::Body;
+    use axum::http::header::CONTENT_TYPE;
+    use axum::http::StatusCode;
+    use axum::response::{IntoResponse, Response};
+    use stpl::html::RenderExt;
+    pub use stpl::html::{
+        a, b, blockquote, body, button, div, doctype, footer, form, h1, h2, h3, h4, h5, head, html,
+        i, img, input, label, li, link, main, meta, nav, ol, p, pre, raw, script, section, span,
+        tbody, textarea, th, thead, tr, tt, u, ul,
+    };
+    pub use stpl::Render;
+
+    pub struct Html(pub Box<dyn Render>);
+
+    impl Display for Html {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.write_fmt(format_args!("{}", self.0.render_to_string()))
+        }
+    }
+
+    pub fn render(children: impl Render + 'static) -> Html {
+        Html(Box::new(children))
+    }
+
+    impl IntoResponse for Html {
+        fn into_response(self) -> axum::response::Response {
+            Response::builder()
+                .status(StatusCode::OK)
+                .header(CONTENT_TYPE, "text/html")
+                .body(Body::from(self.to_string()))
+                .unwrap()
+        }
+    }
+}
+
+pub fn serve(app: App, ip: &str) {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(async {
+            let listener = tokio::net::TcpListener::bind(ip).await.unwrap();
+            println!("Listening on {}", ip);
+            axum::serve(listener, app.router).await.unwrap();
+        });
+}
+
+pub struct App {
+    router: Router,
+}
+
+pub fn app() -> App {
+    App {
+        router: Router::new(),
+    }
+}
+
+impl App {
+    pub fn routes(mut self, router: Router) -> Self {
+        self.router = router;
+        self
+    }
+}
+
+// async fn file(uri: Uri) -> impl IntoResponse {
+//     StaticFile(uri.path().to_string())
+// }
+
+// #[derive(rust_embed::RustEmbed)]
+// #[folder = "static"]
+// #[prefix = "/static/"]
+// pub struct StaticFiles;
+
+// struct StaticFile<T>(T, Box<dyn StaticFiles + 'static>);
+
+// impl<T> IntoResponse for StaticFile<T>
+// where
+//     T: Into<String>,
+// {
+//     fn into_response(self) -> Response {
+//         let path = self.0.into();
+
+//         match StaticFile::get(path.as_str()) {
+//             Some(content) => {
+//                 let mime = mime_guess::from_path(path).first_or_octet_stream();
+//                 ([(header::CONTENT_TYPE, mime.as_ref())], content.data).into_response()
+//             }
+//             None => (StatusCode::NOT_FOUND, "404 Not Found").into_response(),
+//         }
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {}
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,135 @@
+use dubs::{app, html::Html, serve, Routes};
+use view::*;
+
 fn main() {
-    println!("Hello, world!");
+    let app = app().routes(Route::new()); //.static_files(StaticFile::new());
+    serve(app, "127.0.0.1:9001");
 }
+
+async fn root() -> Html {
+    render_root(Route::Root)
+}
+
+#[derive(Routes)]
+enum Route {
+    #[get("/")]
+    Root,
+}
+
+mod view {
+    use super::*;
+    use dubs::html::{self, *};
+
+    pub fn render_root(s: Route) -> Html {
+        render(h1.class("text-2xl")(s.to_string()))
+    }
+
+    fn render(inner: impl Render + 'static) -> Html {
+        html::render((
+            doctype("html"),
+            html((
+                head(link.rel("stylesheet").href("/static/tailwind.css")),
+                body(inner),
+            )),
+        ))
+    }
+}
+
+// #[derive(Serialize, Deserialize)]
+// #[serde(crate = "dubs")]
+// // #[params]
+// struct SetForm {
+//     name: String,
+//     reps: u16,
+//     weight: u16,
+// }
+
+// async fn set_form(_: User, Query(params): Query<SetForm>) -> Html {
+//     render_set_form(params)
+// }
+
+// async fn set(user: User, Json(params): Json<SetForm>) -> Html {
+//     let db = db();
+//     let { sets } = db;
+//     let set: Set = params.into();
+//     set.user = user;
+//     let _ = db.insert_into(sets).values(set).rows_affected().await?;
+//     let sets: Vec<Set> = db.select().from(sets).r#where(eq(sets.user_id, user.id)).limit(30).order(desc(sets.created_at)).all().await?;
+//     render_sets(sets)
+// }
+
+// mod view {
+//     use super::*;
+
+//     pub fn render_root(s: Route) -> Html {
+//         render(h1.class("text-2xl")(s.to_string()))
+//     }
+
+//     pub fn render_set_form(params: SetForm) -> Html {
+//         hype(form((
+//             input.r#type("text").name("name").value(params.name),
+//             input
+//                 .r#type("number")
+//                 .name("reps")
+//                 .value(params.reps.to_string()),
+//             input
+//                 .r#type("number")
+//                 .name("weight")
+//                 .value(params.reps.to_string()),
+//         )))
+//     }
+
+//     pub fn render_sets(sets: Vec<Set>) -> Html {
+//         hype(ul(sets.iter().map(render_set).collect::<Vec<Html>>()))
+//     }
+
+//     pub fn render_set(set: Set) -> Html {
+//         hype
+//     }
+
+//     fn render(inner: impl Render + 'static) -> Html {
+//         hype((
+//             doctype("html"),
+//             html((
+//                 head(link.rel("stylesheet").href("/static/tailwind.css")),
+//                 body(inner),
+//             )),
+//         ))
+//     }
+// }
+
+// // #[db]
+// struct Database {
+//     // users: Users,
+// }
+
+// // #[table]
+// // struct Users {
+// //     #[db(pk, not_null)]
+// //     id: Integer,
+// //     #[db(unique, not_null)]
+// //     secret: Text,
+// //     #[db(not_null)]
+// //     created_at: Integer
+// // }
+
+// // #[row]
+// // struct User {
+// //     id: u64,
+// //     secret: String,
+// //     created_at: u64
+// // }
+
+// static DB: OnceLock<Database> = OnceLock::new();
+// fn db<'a>() -> &'a Database {
+//     DB.get().unwrap()
+// }
+
+// // embed static files in binary
+// // get hash of file contents, append to query string /static/htmx.js?v={hash} when calling them in script/link
+// // #[derive(StaticFiles)]
+// // #[folder = "static"]
+// // enum StaticFile {
+// //     #[name("tailwind.css")]
+// //     Tailwind,
+// // }
